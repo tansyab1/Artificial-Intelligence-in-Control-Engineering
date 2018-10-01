bc6736d95ecd69d2477de1bc111b801d1e42f98d
Sy Nguyen <34137742+tansyab1@users.noreply.github.com>
2018-10-01 22:12:00 +0700

    Update process-model.py

--- ex2-particle-filter/process-model.py
+++ ex2-particle-filter/process-model.py
@@ -6,6 +6,8 @@
 #------------------------------------------------------------------------------
 #	Function represents the process model
 #------------------------------------------------------------------------------
+
+import numpy as np 
 def process_model(x, u, sigma_v, sigma_g):
 	"""
 	[Arguments]
@@ -23,9 +25,30 @@ def process_model(x, u, sigma_v, sigma_g):
 		x_pred : (ndarray) Predicted position, including x coordinate, y
 			coordinate and angle (radian). The shape of x_pred is (3, 1).
 	"""
-	pass
+	 # calculate V* = V + delta(V)
+    new_V = u[0] + sigma_v * np.random.randn()
+    
+    # calculate G* = G + delta(G)
+    new_G = u[1] + sigma_g * np.random.randn()
+    
+    # calculate new angle 
+    new_angle = x[2] + new_V * time_step * np.sin(new_G)/ wb
+    
+    # calculate y
+    new_y = x[1]+ new_V * time_step * np.sin(new_G + new_angle)
+    
+    # calculate x
+    new_x = x[0] + new_V * time_step * np.cos(new_G + new_angle)
+    
+    # result
+    new_coordinates=np.array([[new_x],[new_y],[new_angle]])
+    return new_coordinates
 
 
 #------------------------------------------------------------------------------
 #	Test bench to verify the written function
 #------------------------------------------------------------------------------
+x= np.array([1,1,1])
+u= [2,3]
+a= process_model(x,u, 0.5,0.5,4,1)
+print(a)
