7ceba49c88d58fad1acd547e54ba02c02262260e
dotieuthien <dotieuthien9997@gmail.com>
2018-10-01 20:30:11 +0700

    done

--- ex2-particle-filter/important-factor.py
+++ ex2-particle-filter/important-factor.py
@@ -1,12 +1,22 @@
 #------------------------------------------------------------------------------
 #	Libraries
 #------------------------------------------------------------------------------
-
+import numpy as np 
+from scipy.io import loadmat
 
 #------------------------------------------------------------------------------
 #	Function represents the process model
 #------------------------------------------------------------------------------
-def compute_important_factor(x, z, sigma_r, sigma_b):
+def distribution(x, z, sigma_r, sigma_b):
+	cov_matrix = np.array([[pow(sigma_r, 2), 0], [0, pow(sigma_b, 2)]])
+	det = np.linalg.det(cov_matrix) 
+	inv_cov_matrix = np.linalg.inv(cov_matrix)  
+	part_1 = 1/(2*np.pi*np.sqrt(det)) 
+	part_2 = np.matmul(np.transpose((x - z)), inv_cov_matrix) 
+	part_3 = np.matmul(part_2, (x - z)) 
+	return part_1*np.exp(- part_3)[0][0] 
+
+def compute_important_factor(x, z, lm, sigma_r, sigma_b):
 	"""
 	[Arguments]
 		x : (ndarray) Current position, including x coordinate, y coordinate,
@@ -23,9 +33,31 @@ def compute_important_factor(x, z, sigma_r, sigma_b):
 	[Return]
 		w : (float) Important factor.
 	"""
-	pass
-
+	importance_factor = [] 
+	for i in range(5):
+		if not np.isnan(z[2,i]):
+			id_lm = int(z[2,i] - 1) 
+			xL = lm[0, id_lm]
+			yL = lm[1, id_lm]
+			xt = x[0] 
+			yt = x[1] 
+			phi = x[2] 
+			r = np.sqrt(pow(xt - xL, 2) + pow(yt - yL, 2))  
+			b = np.arctan((yt - yL)/(xt - xL) - phi) 
+			z_pre = np.array([[r], [b]])
+			zt = np.array([[z[0,i]], [z[1,i]]]) 
+			w = distribution(z_pre, zt, sigma_r, sigma_b) 
+			importance_factor.append(w) 
+	return importance_factor
 
 #------------------------------------------------------------------------------
 #	Test bench to verify the written function
 #------------------------------------------------------------------------------
+data = loadmat("data20171107.mat") 
+lm = data["lm"] 
+z = data["Z"]
+z = z[:,:,31]
+x = data["XODO"]
+x = x[:,31] 
+w = compute_important_factor(x, z, lm, 0.2, 2) 
+print(w) 
--- ex2-particle-filter/random-index.py
+++ ex2-particle-filter/random-index.py
@@ -1,7 +1,7 @@
 #------------------------------------------------------------------------------
 #	Libraries
 #------------------------------------------------------------------------------
-
+import numpy as np 
 
 #------------------------------------------------------------------------------
 #	Function represents the process model
@@ -14,9 +14,22 @@ def random_index(W):
 	[Return]
 		idx : (int) Random index based on the given distribution of W.
 	"""
-	pass
-
+	total = np.sum(W) 
+	size = len(W)  
+	p = W/total 
+	return np.random.choice(size, 1, p=p)[0] 
 
 #------------------------------------------------------------------------------
 #	Test bench to verify the written function
 #------------------------------------------------------------------------------
+x = [10,1,3,4,5,5,5,5,5,6,6,7,7,7,3,9] 
+total = np.sum(x) 
+p = x/total
+print(p) 
+y = [] 
+for i in range(100):
+	y.append(random_index(x)) 
+_, count = np.unique(y, return_counts=True)
+total = np.sum(count) 
+p = count/total
+print(p) 
