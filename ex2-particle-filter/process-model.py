3f11e6bda88388b5e087065a19598a173c9c91e2
Sy Nguyen <34137742+tansyab1@users.noreply.github.com>
2018-10-01 22:15:33 +0700

    Update process-model.py
    done

--- ex2-particle-filter/process-model.py
+++ ex2-particle-filter/process-model.py
@@ -6,7 +6,7 @@
 #------------------------------------------------------------------------------
 #	Function represents the process model
 #------------------------------------------------------------------------------
-def process_model(x, u, sigma_v, sigma_g):
+def process_model(x,u, sigma_v, sigma_g , wb, time_step):
 	"""
 	[Arguments]
 		x : (ndarray) Current position, including x coordinate, y coordinate,
@@ -23,9 +23,30 @@ def process_model(x, u, sigma_v, sigma_g):
 		x_pred : (ndarray) Predicted position, including x coordinate, y
 			coordinate and angle (radian). The shape of x_pred is (3, 1).
 	"""
-	pass
+	# calculate V* = V + delta(V)
+    new_V = u[0] + sigma_v * np.random.randn()
+    
+    # calculate G* = G + delta(G)
+    new_G = u[1] + sigma_g * np.random.randn()
+    
+    # calculate new angle 
+    new_angle = x[2] + new_V * time_step * np.sin(new_G)/ wb
+    
+    # calculate y
+    new_y = x[1]+ new_V * time_step * np.sin(new_G + new_angle)
+    
+    # calculate x
+    new_x = x[0] + new_V * time_step * np.cos(new_G + new_angle)
+    
+    # result
+    new_coordinates=np.array([[new_x],[new_y],[new_angle]])
+    return new_coordinates
 
 
 #------------------------------------------------------------------------------
 #	Test bench to verify the written function
 #------------------------------------------------------------------------------
+x= np.array([1,1,1]) # current location
+u= [2,3] # control signal
+a= process_model(x,u, 0.5,0.5,4,1)
+print(a) # result
